23ebe04d26e66a835ec5e88f5bab39b492170e7d
mahlokwa <mphkam016@myuct.ac.za>
2026-03-24 23:49:50 +0200

    update db connection to use environment variables

--- db/connection.js
+++ db/connection.js
@@ -1,11 +1,13 @@
 import mysql from "mysql2";
+import dotenv from "dotenv";
+dotenv.config();
 
 const db = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "Kamo@2000", // your MySQL password
-  database: "sna_driving_school",
-  port: 3306
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306
 });
 
 // Test the connection
@@ -14,7 +16,7 @@ db.getConnection((err, connection) => {
     console.error("Error connecting to MySQL:", err);
   } else {
     console.log("Connected to MySQL!");
-    connection.release(); // release back to pool
+    connection.release();
   }
 });
 
