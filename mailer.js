a0d8b49f5ef016d4b82ce9a231c219dea0e9cf9b
mahlokwa <mphkam016@myuct.ac.za>
2026-04-01 13:36:15 +0200

    updated  mailer

--- mailer.js
+++ mailer.js
@@ -6,7 +6,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendTokenEmail = async (toEmail, fullName, bookingToken) => {
   await resend.emails.send({
-    from: 'SNA Driving School - No Reply <onboarding@resend.dev>',
+    from: 'SNA Driving School - No Reply <noreply@snadriving.co.za>',
     to: toEmail,
     subject: 'Your SNA Driving School Booking Token',
     html: `
